574b169f76cb6943e5ab5283b1d3cb183bc7aa7c
Namixnaho0 <nahomi.ap1512@gmail.com>
2026-09-01 12:48:34 -0600

    creacion de expedientes digitales automaticos para prospectos agregados manualmente

--- Digitales/signals.py
+++ Digitales/signals.py
@@ -2,6 +2,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from citas.models import ClienteComercial
 from .models import ExpedienteDigital, MensajeWhatsApp
 
 ESTADO_SIN_CONTACTAR = "Sin Contactar"
@@ -27,3 +28,13 @@ def marcar_primer_contacto_humano(sender, instance, created, **kwargs):
         cliente__telefono=instance.telefono,
         estado=ESTADO_SIN_CONTACTAR,
     ).update(estado=ESTADO_CONTACTADO)
+
+
+@receiver(post_save, sender=ClienteComercial)
+def crear_expediente_digital_si_no_existe(sender, instance, created, **kwargs):
+    if not created:
+        return
+    ExpedienteDigital.objects.get_or_create(
+        cliente=instance,
+        defaults={"agencia": "", "asesor_digital": ""},
+    )
